0a7a0959e91014c6d3372e3a9a2ed35afbec582c
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-01-15 18:26:39 +0200

    configurando reactquery

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -6,10 +6,11 @@ import {
   TimePickerItem,
   TimePickerList,
 } from './styles'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import dayjs from 'dayjs'
 import { api } from '@/lib/axios'
 import { useRouter } from 'next/router'
+import { useQuery } from '@tanstack/react-query'
 
 interface Availability {
   possibleTimes: number[]
@@ -18,7 +19,6 @@ interface Availability {
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-  const [availability, setAvailability] = useState<Availability | null>(null)
 
   const router = useRouter()
 
@@ -29,21 +29,30 @@ export function CalendarStep() {
     ? dayjs(selectedDate).format('DD[ de ]MMMM')
     : null
 
-  useEffect(() => {
-    if (!selectedDate) {
-      return
-    }
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format('YYYY-MM-DD')
+    : null
 
-    api
-      .get(`/users/${String(router.query.username)}/availability`, {
-        params: {
-          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+  const { data: availability } = useQuery<Availability>({
+    queryKey: [
+      'availability',
+      String(router.query.username),
+      selectedDateWithoutTime,
+    ],
+    queryFn: async () => {
+      const response = await api.get(
+        `/users/${String(router.query.username)}/availability`,
+        {
+          params: {
+            date: selectedDateWithoutTime,
+          },
         },
-      })
-      .then((response) => {
-        setAvailability(response.data)
-      })
-  }, [selectedDate, router.query.username])
+      )
+
+      return response.data
+    },
+    enabled: !!selectedDate,
+  })
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
